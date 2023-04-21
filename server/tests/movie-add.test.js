f9b02a7959b2026c45895f0a560a42538d46ee3e
Alejandro De Luca <tury3001@gmail.com>
2023-04-21 12:16:41 +0200

    Move movie data sample to a new module

--- server/tests/movie-add.test.js
+++ server/tests/movie-add.test.js
@@ -6,16 +6,23 @@ const Country = require('../database/models/Country')
 const Language = require('../database/models/Language')
 const { countryData } = require('../database/seeders/seed-country')
 const { languageData } = require('../database/seeders/seed-language')
+const getMovieData = require('./samples/movie-data-sample')
 
 const app = server.getApp()
 
-beforeAll( async () => {
+let movieData
+
+beforeAll( async () => {  
   await Country.insertMany(countryData())
   await Language.insertMany(languageData())
 })
 
 afterAll(async () => {
   await dbDisconnect()
+})
+
+beforeEach(() => {
+  movieData = getMovieData()
 })
 
 afterEach(async () => {
@@ -33,7 +40,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with empty title', async () => {
-    const movieData = getMovieData()
+    
     movieData.title = ''
 
     await request(app)
@@ -48,7 +55,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with a title greater than the max limit', async () => {
-    const movieData = getMovieData()
+    
     movieData.title = 'E'.repeat(81)
 
     await request(app)
@@ -63,7 +70,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with empty year', async () => {
-    const movieData = getMovieData()
+    
     movieData.year = null
 
     await request(app)
@@ -78,7 +85,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with non numeric year', async () => {
-    const movieData = getMovieData()
+    
     movieData.year = 'this is not a year'
 
     await request(app)
@@ -93,7 +100,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with a year less than 1895', async () => {
-    const movieData = getMovieData()
+    
     movieData.year = 1894
 
     await request(app)
@@ -108,7 +115,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with empty director', async () => {
-    const movieData = getMovieData()
+    
     movieData.director = ''
 
     await request(app)
@@ -120,7 +127,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with a director string longer than 60 characters', async () => {
-    const movieData = getMovieData()
+    
     movieData.director = 'E'.repeat(81)
 
     await request(app)
@@ -135,7 +142,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with empty set of genres', async () => {
-    const movieData = getMovieData()
+    
     movieData.genres = []
 
     await request(app)
@@ -147,7 +154,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with empty genres', async () => {
-    const movieData = getMovieData()
+    
     movieData.genres = ['western', null]
 
     await request(app)
@@ -162,7 +169,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with invalid genres', async () => {
-    const movieData = getMovieData()
+    
     movieData.genres = [4233, 12]
 
     await request(app)
@@ -177,7 +184,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with empty synopsis', async () => {
-    const movieData = getMovieData()
+    
     movieData.synopsis = ''
 
     await request(app)
@@ -189,7 +196,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with invalid synopsis', async () => {
-    const movieData = getMovieData()
+    
     movieData.synopsis = { some: 'thing' }
 
     await request(app)
@@ -204,7 +211,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with synopsis that has more than 512 characters', async () => {
-    const movieData = getMovieData()
+    
     movieData.synopsis = 's'.repeat(513)
 
     await request(app)
@@ -219,7 +226,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with empty comment', async () => {
-    const movieData = getMovieData()
+    
     movieData.comment = ''
 
     await request(app)
@@ -233,7 +240,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with invalid comment', async () => {
-    const movieData = getMovieData()
+    
     movieData.comment = { some: 'thing ' }
 
     await request(app)
@@ -248,7 +255,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with comment that has more than 512 characters', async () => {
-    const movieData = getMovieData()
+    
     movieData.comment = 'r'.repeat(513)
 
     await request(app)
@@ -263,7 +270,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with empty tags', async () => {
-    const movieData = getMovieData()
+    
     movieData.tags = []
 
     await request(app)
@@ -275,7 +282,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with invalid tags', async () => {
-    const movieData = getMovieData()
+    
     movieData.tags = { some: 'data' }
 
     await request(app)
@@ -290,7 +297,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with a tag which type is invalid', async () => {
-    const movieData = getMovieData()
+    
     movieData.tags = [2432, { asdas: 'asd' }]
 
     await request(app)
@@ -305,7 +312,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with one or more empty tags', async () => {
-    const movieData = getMovieData()
+    
     movieData.tags = ['classic', '']
 
     await request(app)
@@ -320,7 +327,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with a tag with more than 60 characters', async () => {
-    const movieData = getMovieData()
+    
     movieData.tags = ['classic', 's'.repeat(61)]
 
     await request(app)
@@ -334,8 +341,7 @@ describe('add movie tests', () => {
     expect(await Movie.count()).toBe(0)
   })
 
-  test('add a new movie links the movie with the country', async () => {
-    const movieData = getMovieData()
+  test('add a new movie links the movie with the country', async () => {    
 
     await request(app)
       .post('/api/movie')
@@ -349,7 +355,7 @@ describe('add movie tests', () => {
   })
 
   test('add a new movie with more than one country links the movie with every country', async () => {
-    const movieData = getMovieData()
+    
     movieData.countries = ['Argentina', 'Spain']
 
     await request(app)
@@ -365,7 +371,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie without country', async () => {
-    const movieData = getMovieData()
+    
     movieData.countries = []
 
     await request(app)
@@ -379,7 +385,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with a country that does not exist', async () => {
-    const movieData = getMovieData()
+    
     movieData.countries = ['Transilvania']
 
     await request(app)
@@ -392,7 +398,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with a duplicated country', async () => {
-    const movieData = getMovieData()
+    
     movieData.countries = ['Argentina', 'Spain', 'Spain', 'Argentina']
 
     await request(app)
@@ -408,7 +414,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie without language', async () => {
-    const movieData = getMovieData()
+    
     movieData.languages = []
 
     await request(app)
@@ -417,8 +423,7 @@ describe('add movie tests', () => {
       .expect(201)
   })
 
-  test('add new movie with one language', async () => {
-    const movieData = getMovieData()
+  test('add new movie with one language', async () => {    
     
     await request(app)
       .post('/api/movie')
@@ -433,7 +438,6 @@ describe('add movie tests', () => {
 
   test('add new movie with multiple languages', async () => {
 
-    const movieData = getMovieData()
     movieData.languages = ['English', 'French']
     
     await request(app)
@@ -449,7 +453,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with duplicated languages', async () => {
-    const movieData = getMovieData()
+    
     movieData.languages = ['English', 'Spanish', 'English', 'Spanish', 'French']
 
     await request(app)
@@ -466,7 +470,7 @@ describe('add movie tests', () => {
   })
 
   test('add new movie with duplicated languages', async () => {
-    const movieData = getMovieData()
+    
     movieData.languages = ['Ergllish']
 
     await request(app)
@@ -476,22 +480,5 @@ describe('add movie tests', () => {
       .expect( (res) => {
         expect(res.body.message).toBe('Given language doesn\'t exist')
       })
-  })
-
-
-
-
-  function getMovieData () {
-    return {
-      title: 'Jurassic Park',
-      year: 1993,
-      director: 'Steven Spielberg',
-      genres: ['adventure', 'sci-fi'],
-      countries: ['United States'],
-      languages: ['English'],
-      synopsis: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat qui quis est quibusdam architecto harum provident aspernatur odit. Iste id unde asperiores modi ea quam ab nulla aliquid odio! Maxime.',
-      comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam cumque quod vel minima fuga eligendi fugit amet, voluptatem id omnis corporis.',
-      tags: ['classic', 'film-noir']
-    }
-  }
+  })  
 })
---
+++ server/tests/samples/movie-data-sample.js
@@ -0,0 +1,15 @@
+function getMovieData() {
+  return {
+    title: 'Jurassic Park',
+    year: 1993,
+    director: 'Steven Spielberg',
+    genres: ['adventure', 'sci-fi'],
+    countries: ['United States'],
+    languages: ['English'],
+    synopsis: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat qui quis est quibusdam architecto harum provident aspernatur odit. Iste id unde asperiores modi ea quam ab nulla aliquid odio! Maxime.',
+    comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam cumque quod vel minima fuga eligendi fugit amet, voluptatem id omnis corporis.',
+    tags: ['classic', 'film-noir']
+  }
+}
+
+module.exports = getMovieData;
